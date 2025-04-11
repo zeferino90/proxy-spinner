9e2ed9f6f91ee47113c52b538f22a14ad4ad6801
Eric Cañas <elcorreodeharu@gmail.com>
2025-04-11 21:19:14 +0200

    Update to 0.5

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='proxy-spinner',
-    version='0.4',
+    version='0.5',
     author='Eric-Canas',
     author_email='eric@ericcanas.com',
     url='https://github.com/Eric-Canas/proxy-spinner',
